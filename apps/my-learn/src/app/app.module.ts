34dadcbd45358c8c9c90c7a9969f53d3ffba8d1c
Orxan-Ibrahimov <orxan.ibrahimli.98@gmail.com>
2024-01-06 02:41:10 +0400

    ProjectLaunchedAndMenuComponentAdded

--- apps/my-learn/src/app/app.module.ts
+++ apps/my-learn/src/app/app.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { MenuComponent } from './shared/menu/menu.component';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
+  declarations: [
+    AppComponent,
+    MenuComponent
+  ],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
