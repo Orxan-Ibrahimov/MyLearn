8779cccff8d4e5e140806471cd52ed861b142f85
Orxan-Ibrahimov <orxan.ibrahimli.98@gmail.com>
2024-01-15 19:35:19 +0400

    QuickOptionsComponentAdded

--- apps/my-learn/src/app/app.module.ts
+++ apps/my-learn/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { MenuComponent } from './shared/menu/menu.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { HeaderComponent } from './shared/header/header.component';
+import { EducationOptionComponent } from './pages/education-option/education-option.component';
 
 @NgModule({
   declarations: [
@@ -12,6 +13,7 @@ import { HeaderComponent } from './shared/header/header.component';
     MenuComponent,
     HeaderComponent,
     FooterComponent,
+    EducationOptionComponent
   ],
   imports: [BrowserModule],
   providers: [],
---
+++ apps/my-learn/src/app/pages/education-option/education-option.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "online-education-option",
+    templateUrl: "./education-option.component.html"
+})
+export class EducationOptionComponent{
+
+}
